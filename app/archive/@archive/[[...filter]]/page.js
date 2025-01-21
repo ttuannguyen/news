8b5942c2b6388bc73929b961965a896615ed85bc
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-01-21 17:49:08 -0500

    delete extra page.js file in archive

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,9 +1,26 @@
 import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
+import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import Link from "next/link";
 
 export default function FilteredNewsPage({ params }) {
-    const newsYear = params.filter;
-    const news = getNewsForYear(newsYear);
+  const filter = params.filter;
+  const news = getNewsForYear(newsYear);
 
-    return <NewsList news={news} />
+  const links = getAvailableNewsYears();
+
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+
+  //<NewsList news={news} />
 }
