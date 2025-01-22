a50e3688aa50b66f0aacc71f64d2c3b5c4d50916
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-01-21 21:08:00 -0500

    update conditional to make year nav disappear if there is a year-month combination

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9c0febce400b8cfd")
+/******/ 		__webpack_require__.h = () => ("d6eb8570c57e77ab")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -24,6 +24,7 @@ export default function FilteredNewsPage({ params }) {
 
   if (selectedYear && selectedMonth) {
     news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
   }
 
   let newsContent = <p>No news found for selected period.</p>;
